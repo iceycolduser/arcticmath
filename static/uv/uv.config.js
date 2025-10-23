92db56611551666cf470196d8f49b127f29c6032
arcticmath <arcticmath@gmail.com>
2025-10-22 21:07:14 -0500

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,7 +1,5 @@
 self.__uv$config = {
     prefix: '/service/',
-    // The SW-side Ultraviolet config also needs to know the bare host so it will construct websocket URLs
-    bare: 'https://vercbareserver.vercel.app/seal/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
