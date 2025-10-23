39e7d6fa45b736719a88c1d41cc815a93a6d89e3
arcticmath <arcticmath@gmail.com>
2025-10-23 18:52:00 -0500

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: '/bare/',  // REQUIRED: Your Bare server endpoint
+    bare: '/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
