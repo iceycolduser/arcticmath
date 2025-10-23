cbaa2f7d9175b9cb109c0e2ab89d85bc3853e90a
arcticmath <arcticmath@gmail.com>
2025-10-22 20:46:31 -0500

    Create Application.java

---
+++ src/main/java/com/mathpunch/wsproxy/WebSocketProxy/Application.java
@@ -0,0 +1,12 @@
+package com.mathpunch.wsproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSocketProxyApplication.class, args);
+    }
+}
