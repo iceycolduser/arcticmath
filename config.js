38b79290dc6db886d09ba891dc8b162aa0f91d21
arcticmath <arcticmath@gmail.com>
2025-10-23 21:10:10 -0500

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  challenge: false, // Set to true if you want to enable password protection.
-  users: {
-    // You can add multiple users by doing username: 'password'.
-    Space: "password",
-  },
-};
-
-export default config;
