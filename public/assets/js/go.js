9e96aa2d557aa15263ccc7f7950acb95f57379a0
arcticmath <arcticmath@gmail.com>
2025-10-26 10:48:42 -0500

    Delete public directory

--- public/assets/js/go.js
+++
@@ -1,6 +0,0 @@
-const storedUrl = localStorage.getItem('url');
-
-if (storedUrl) {
-    const iframe = document.getElementById('iframe');
-    iframe.src = storedUrl;
-}
--- public/assets/js/inject.js
+++
@@ -1,1 +0,0 @@
-console.log('%cAstro', 'color: #037ef9; font-size: 100px; font-family: "Poppins", sans-serif; font-weight:bold;');
